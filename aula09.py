ead1a7d2f899752835b220420896eab39b8e4288
Samuel Evangelista <samueo467@gmail.com>
2023-04-03 11:00:37 -0300

    aula09

---
+++ aula09.py
@@ -0,0 +1,26 @@
+frase  = 'Curso em Video Python'
+print(frase[3])
+print(frase[3:13])
+print(frase[:13])
+print(frase[13:])
+print(frase[1:15:2])
+print(frase[1::2])
+print(frase[::2])
+print("""         D#m7(5-)
+Ouça-me bem amor
+            E/D               C#m7
+Preste atenção o mundo é um moinho
+          Cº                       Bm7
+Vai triturar teus sonhos, tão mesquinhos
+         E7               Bbº  F#7  F#7/E
+Vai reduzir as ilusões a pó """)
+print(frase.count('o'))
+print(frase.count('O'))
+print(frase.upper().count('O'))
+print(len[frase])
+print(len[frase.strip()])
+print(frase.replace('Python', 'Android'))
+print('Curso' in frase)
+print(frase.find('Curso'))
+print(frase.lower().find('video'))
+
