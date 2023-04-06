acbd45c3346ec33cda69f207c5048f8836bd7b80
Samuel Evangelista <samueo467@gmail.com>
2023-04-06 10:57:32 -0300

    Create aula010.py

---
+++ aula010.py
@@ -0,0 +1,9 @@
+n1=float(input('Digite a primeira nota: '))
+n2=float(input('Digite a segunda nota: '))
+m = (n1 + n2)/2
+print('A sua média foi {:.1f}'.foramt(m))
+if m >= 6:
+    print('voc~e passou!')
+else:
+    print('Vc reprovou!')
+    
