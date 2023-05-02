75a7aa094d724a78bbf5194238d50089c36c6772
Samuel Evangelista <samueo467@gmail.com>
2023-05-02 08:46:12 -0300

    aula015

---
+++ aula015.py
@@ -0,0 +1,15 @@
+'''cont = 1
+while True:
+    print(cont, '-> ', end='')
+    cont += 1
+    if cont == 100:
+        break
+print('Acabou')'''
+n = s = 0
+while True:
+    n = int(input('Digite um número: '))
+    if n == 999:
+        break
+    s += n
+# print('A soma vale {}'.format(s))
+print(f'A soma vale {s}')
