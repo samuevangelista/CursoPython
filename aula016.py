42fba91768a7d770c888f7fe1617006b95f323d9
Samuel Evangelista <samueo467@gmail.com>
2023-05-04 10:32:17 -0300

    04/05

---
+++ aula016.py
@@ -0,0 +1,17 @@
+'''lanche = ('Hamburguer', 'Suco', 'Pizza', 'Pudim')
+for comida in lanche:
+    print(f'Eu vou comer {comida}')
+
+for cont in range(0, len(lanche)):
+    print(f'Eu vou comer {lanche[cont]} na posição {cont}')
+
+for pos, comida in enumerate(lanche):
+    print(f'Eu vou comer {comida} na posiçao {pos}')
+print(sorted(lanche))
+print(lanche)'''
+
+a = (2, 5, 4)
+b = (5, 8, 1, 2)
+c = a + b
+print(c)
+print(c.index(8))
