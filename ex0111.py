b269697b091328b03b62eee6eec3fe724248d8be
Samuel Evangelista <samueo467@gmail.com>
2023-05-08 08:22:23 -0300

    08/05

---
+++ ex0111.py
@@ -0,0 +1,5 @@
+from random import randint
+nomes = ('Samuel', 'Guilherme', 'Antonio', 'Ranielison',
+         'Luiz', 'Fernando', 'Bibito', 'Eliel', 'Magno', 'Gustavo')
+escolha = randint(1,10)
+print(escolha)
