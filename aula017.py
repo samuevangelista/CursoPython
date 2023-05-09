8effc57a8cb8dfea1dada64978c39d2ccbd4540c
Samuel Evangelista <samueo467@gmail.com>
2023-05-09 11:00:11 -0300

    09/05

--- aula017.py
+++ aula017.py
@@ -1,16 +1,16 @@
-'''num = [2, 5, 9, 1]
+num = [2, 5, 9, 1]
 num[2] = 3
 num.append(7)
-num.sort(reverse=True)
-num.insert(2, 2)
-if 4 in num:
+#num.sort(reverse=True)
+#num.insert(2, 2)
+'''if 4 in num:
     num.remove(4)
 else:
     print('Não achei o numero')
 print(num)
 print(f'Esse lista tem {len(num)} elementos')'''
-valores = []
-'''valores.append(5)
+'''valores = []
+valores.append(5)
 valores.append(9)
 valores.append(4)'''
 
@@ -22,8 +22,8 @@ for c, v in enumerate(valores):
     print(f'Na posição {c} encontrei o valor {v}!')
 print('cheguei no final')'''
 
-a = [2, 3, 4, 7]
+'''a = [2, 3, 4, 7]
 b = a[:]#!=b = a
 b[2] = 8
 print(f'Lista A: {a}')
-print(f'Lista B: {b}')
+print(f'Lista B: {b}')'''
